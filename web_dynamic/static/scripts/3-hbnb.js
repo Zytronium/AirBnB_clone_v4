a7b8fd957d457ea11a35a3f19711892abfa247e2
Zytronium <dastelljes@outlook.com>
2024-12-01 18:23:48 -0600

    updated 3-hbnb.js

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -20,4 +20,29 @@ $(function () {
             $("#api_status").removeClass("available");
         }
     });
+
+    const placesApiUrl = "http://0.0.0.0:5001/api/v1/places_search/";
+    $.ajax({
+        url: placesApiUrl,
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify({}),
+        success: function (data) {
+            $(".places").empty();
+            data.forEach((place) => {
+                const article = `
+                    <article>
+                        <h2>${place.name}</h2>
+                        <div class="price_by_night">$${place.price_by_night}</div>
+                        <div class="information">
+                            <div class="max_guest">${place.max_guest} Guests</div>
+                            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                        </div>
+                        <div class="description">${place.description}</div>
+                    </article>`;
+                $(".places").append(article);
+            });
+        }
+    });
 });
